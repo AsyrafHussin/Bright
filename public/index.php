d84f30b3deb9f76e0372cb9bbe667794da9f8da2
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-24 12:54:02 +0800

    Replace autoload function with composer autoloader

--- public/index.php
+++ public/index.php
@@ -5,17 +5,6 @@
  */
 require __DIR__.'/../vendor/autoload.php';
 
-/**
- * Autoload
- */
-spl_autoload_register(function($class){
-   $root = dirname(__DIR__); // get the parent directory
-   $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-   if(is_readable($file)){
-        require $file;
-   } 
-});
-
 $router = new Core\Router();
 
 //Add the routes
