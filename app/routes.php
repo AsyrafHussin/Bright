56b33a772267a9ab7e9021a66d27d09fafd5653a
Asyraf Hussin <asyrafhussin4@gmail.com>
2017-09-25 21:48:21 +0800

    Seperate routes from index file

---
+++ app/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Routes
+ */
+
+$router = new Bright\Routing\Router();
+
+//Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+$router->add('sub/{controller}/{action}', ['namespace' => 'Sub']);
+    
+$router->dispatch($_SERVER["REQUEST_URI"]);
--- public/index.php
+++ public/index.php
@@ -5,12 +5,5 @@
  */
 require __DIR__.'/../vendor/autoload.php';
 
-$router = new Bright\Routing\Router();
-
-//Add the routes
-$router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('{controller}/{action}');
-$router->add('{controller}/{id:\d+}/{action}');
-$router->add('sub/{controller}/{action}', ['namespace' => 'Sub']);
-    
-$router->dispatch($_SERVER["REQUEST_URI"]);
+// include router
+require 'app/routes.php';
